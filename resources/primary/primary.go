bcf4c31aa75e5fe7a94f07ac4fbe2400470e197b
Marcin Kaciuba <aldor007@gmail.com>
2021-04-07 11:33:55 +0200

    feat: working on primary deployment

--- resources/primary/primary.go
+++ resources/primary/primary.go
@@ -3,6 +3,7 @@ package resources
 import (
 	"context"
 
+	mariadbv1alpha1 "github.com/aldor007/mariadb-operator/api/v1alpha1"
 	"github.com/aldor007/mirth-operator/resources/resources"
 	"github.com/go-logr/logr"
 	"k8s.io/apimachinery/pkg/runtime"
@@ -19,13 +20,13 @@ type Reconciler struct {
 	Scheme *runtime.Scheme
 }
 
-func NewPrimary(client client.Client, directClient client.Reader, scheme *runtime.Scheme, cluster *v1alpha1.MariaDBCluster) *Reconciler {
+func NewPrimary(client client.Client, directClient client.Reader, scheme *runtime.Scheme, cluster *mariadbv1alpha1.MariaDBCluster) *Reconciler {
 	return &Reconciler{
 		Scheme: scheme,
 		Reconciler: resources.Reconciler{
-			Client:       client,
-			DirectClient: directClient,
-			KafkaCluster: cluster,
+			Client:         client,
+			DirectClient:   directClient,
+			MariaDBCluster: cluster,
 		},
 	}
 }
@@ -41,10 +42,10 @@ func (r *Reconciler) Reconcile(ctx context.Context, log logr.Logger) error {
 		Namespace: instance.Namespace,
 	}, found)
 	if err != nil && errors.IsNotFound(err) {
-
+		dep := r.createDeployment()
 		// Create the deployment
 		log.Info("Creating a new Deployment", "Deployment.Namespace", dep.Namespace, "Deployment.Name", dep.Name)
-		err = r.client.Create(context.TODO(), dep)
+		err = r.Client.Create(ctx, dep)
 
 		if err != nil {
 			// Deployment failed
@@ -93,4 +94,106 @@ func (r *Reconciler) Reconcile(ctx context.Context, log logr.Logger) error {
 	}
 
 	return nil, nil
+}
+
+func (r *Reconciler) createDeployment() {
+	labels := utils.Labels(v, "mariadb")
+	size := v.Spec.Size
+	image := v.Spec.Image
+
+	dbname := v.Spec.Database
+	rootpwd := v.Spec.Rootpwd
+
+	userSecret := &corev1.EnvVarSource{
+		SecretKeyRef: &corev1.SecretKeySelector{
+			LocalObjectReference: corev1.LocalObjectReference{Name: mysqlAuthName()},
+			Key:                  "username",
+		},
+	}
+
+	passwordSecret := &corev1.EnvVarSource{
+		SecretKeyRef: &corev1.SecretKeySelector{
+			LocalObjectReference: corev1.LocalObjectReference{Name: mysqlAuthName()},
+			Key:                  "password",
+		},
+	}
+
+	dep := &appsv1.Deployment{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      mariadbDeploymentName(v),
+			Namespace: v.Namespace,
+		},
+		Spec: appsv1.DeploymentSpec{
+			Replicas: &size,
+			Selector: &metav1.LabelSelector{
+				MatchLabels: labels,
+			},
+			Template: corev1.PodTemplateSpec{
+				ObjectMeta: metav1.ObjectMeta{
+					Labels: labels,
+				},
+				Spec: corev1.PodSpec{
+					Volumes: []corev1.Volume{
+						{
+							Name: pvStorageName,
+							VolumeSource: corev1.VolumeSource{
+								PersistentVolumeClaim: &corev1.PersistentVolumeClaimVolumeSource{
+									ClaimName: pvClaimName,
+								},
+							},
+						},
+					},
+					Containers: []corev1.Container{{
+						Image:           image,
+						ImagePullPolicy: corev1.PullIfNotPresent,
+						Name:            "mariadb-service",
+						Ports: []corev1.ContainerPort{{
+							ContainerPort: mariadbPort,
+							Name:          "mariadb",
+						}},
+						VolumeMounts: []corev1.VolumeMount{
+							{
+								Name:      pvStorageName,
+								MountPath: "/var/lib/mysql",
+							},
+						},
+						Env: []corev1.EnvVar{
+							{
+								Name:  "MYSQL_ROOT_PASSWORD",
+								Value: rootpwd,
+							},
+						},
+					}},
+				},
+			},
+		},
+	}
+
+	controllerutil.SetControllerReference(v, dep, r.scheme)
+	return dep
+}
+
+func (r *ReconcileMariaDB) mariadbService(v *mariadbv1alpha1.MariaDB) *corev1.Service {
+	labels := utils.Labels(v, "mariadb")
+
+	s := &corev1.Service{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      mariadbServiceName(v),
+			Namespace: v.Namespace,
+			Labels:    labels,
+		},
+		Spec: corev1.ServiceSpec{
+			Selector: labels,
+			Ports: []corev1.ServicePort{{
+				Protocol:   corev1.ProtocolTCP,
+				Port:       mariadbPort,
+				TargetPort: intstr.FromInt(3306),
+				NodePort:   v.Spec.Port,
+			}},
+			Type: corev1.ServiceTypeNodePort,
+		},
+	}
+
+	controllerutil.SetControllerReference(v, s, r.scheme)
+	return s
 }
